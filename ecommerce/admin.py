72ce1bca74ab3a93d6eb8233768d63b09a260890
prince <nguhprince1@gmail.com>
2024-05-23 06:40:50 +0100

    Completed the Django admin section

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import *
 
+admin.AdminSite
+
 class ImageInline(admin.StackedInline):
     model = Image
     extra = 3
@@ -10,11 +12,21 @@ class VariantInline(admin.StackedInline):
     model = Variant
     extra = 3
 
+class CollectionProductInline(admin.StackedInline):
+    model = CollectionProduct
+    extra = 3
+
+class OrderProductInline(admin.StackedInline):
+    model = OrderProduct
+    extra = 3
+
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ImageInline, VariantInline]
+    inlines = [ImageInline, VariantInline, CollectionProductInline]
+    list_display = ("name", "price", "quantity")
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderProductInline]
 
 admin.site.register(Collection)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(CollectionProduct)
-admin.site.register(Order)
-admin.site.register(OrderProduct)
+admin.site.register(Order, OrderAdmin)
